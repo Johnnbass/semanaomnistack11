8672e804d441a44234240167db6042a1533b82c7
Jonatha Silveira <desenvolvimento3@geoconvenio.com>
2020-03-28 00:02:33 -0300

    Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+// em caso de publicação em produção
+// app.use(cors({
+//   origin: 'http://meuapp.com' 
+// }));
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+/**
+ * Rotas / Recursos
+ */
+
+ /**
+  * Métodos HTTP:
+  * 
+  * GET : Buscar/listar uma informação do backend
+  * POST: Criar uma informação no backend
+  * PUT: Alterar uma informação no bakcend
+  * DELETE: Deletar uma informação no backend
+  */
+
+/**
+ * Tipos de parâmetros:
+ * 
+ * Query Params: parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
+ * Route Params: parâmetros utilizados para identificar recursos
+ * Request Body: o corpo da requisição utilizado para criar ou alterar recursos
+ */
+
+/**
+ * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+ */
+
+/**
+ * Driver: SELECT * FROM users
+ * Query Builder: table('users').select('*').where()
+ */
